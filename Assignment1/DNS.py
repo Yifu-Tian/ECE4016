8c122d93c92d058fb5109c5b1a1eeb4361ef4445
Yifu <102942951+Yifu-Tian@users.noreply.github.com>
2023-10-12 21:29:12 +0800

    Add files via upload

---
+++ Assignment1/DNS.py
@@ -0,0 +1,71 @@
+import socket
+import dnslib
+def qnameReverse(qnamelist):
+    n = len(qnamelist)
+    for i in range(n): qnamelist[i] = qnamelist[i] + "."
+    qnamelist.reverse()
+    return qnamelist
+def checkRes(dcm, qnamelist):
+    a = '198.41.0.4'
+    ip_tmp = a
+    domain = ""
+    for i in qnamelist:
+        domain = i + domain
+        print(ip_tmp)
+        r = dnslib.DNSRecord.question(domain)
+        rr = r.send(a)
+        res = dnslib.DNSRecord.parse(rr)
+        if res.auth != []:
+            a = res.auth[0].rdata.__str__()
+            ip_tmp = res.ar[0].rdata.__str__() if (res.ar != []) else query(res.auth[0].rdata, dcm)
+        if res.auth == [] and res.rr != []:
+            if (str(res.rr[0].rdata)[0].isdigit() == True): continue
+            else:
+                for RR in res.rr: dcm.add_answer(RR)
+                res = query(res.rr[0].rdata, dcm)
+    return res
+            
+def query(qin, dcm):
+    qname = str(qin)
+    qnamelist = qname.split(".")
+    qnamelist.pop(len(qnamelist) - 1) 
+    qnamelist = qnameReverse(qnamelist)
+    return checkRes(dcm, qnamelist)
+
+def local_DNS_Server(flag):
+    cache = {}
+    while True:
+        serverSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        serverSocket.bind(('127.0.0.1', 1234))
+        message, clientAddress = serverSocket.recvfrom(2048)
+        message_parse = dnslib.DNSRecord.parse(message)
+        message_parse.header.set_rd(0)
+        message = dnslib.DNSRecord.pack(message_parse)
+        qname = str(message_parse.q.qname)
+
+        if message_parse.q.qname in cache:
+            rr = cache[qname]
+            DCmessage = message_parse.reply()
+            for RR in rr: DCmessage.add_answer(RR)
+        else: 
+            if flag == 0:
+                request = message_parse.send("8.8.8.8")
+                DCmessage = dnslib.DNSRecord.parse(request)
+            else:
+                DCmessage = message_parse.reply()
+                res = query(message_parse.q.qname, DCmessage)
+                for RR in res.rr: DCmessage.add_answer(RR)
+            DCmessage.header.rd = 1
+            cache[qname] = DCmessage.rr
+            serverSocket.sendto(DCmessage.pack(),clientAddress)
+        print("==============================")
+
+def main():
+    flag = int(input("Enter flag 0 or 1: "))
+    local_DNS_Server(flag)
+    return
+if __name__ == '__main__':
+    try:
+        main()
+    except KeyboardInterrupt:
+        pass
